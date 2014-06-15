de3a3f26d8bac279acc98ebb54b0e78c159442c5
EvelynLiu <100703007@nccu.edu.tw>
2014-06-15 19:05:10 +0800

    testtest

--- product.js
+++ product.js
@@ -9,22 +9,33 @@ function getData(page,category){
   var limit = 15;
   var skip = (page-1) * limit;
   var Farmer = Parse.Object.extend("Farmer");
+  var Product = Parse.Object.extend("Product");
   var query = new Parse.Query(Farmer);
-  console.log(query);
+  var queryP = new Parse.Query(Product);
   query.limit(limit);
   query.skip(skip);
   query.equalTo("district", category);
   query.descending("createdAt");
-  console.log(query);
   query.find({
     success: function(results) {
       alert(1);
+      //$('#content').html("");
+      var list = "";
       var objList = results.map(function (e){ return e.toJSON() });
-      objList.forEach(function(e){
-        console.log(e);
+      objList.forEach(function (e){
         //var html = '<div class="about"><img src="'+e.Name+'"></img><p class="name">'+e.Name+'</p><p>'+e.Name+'</p></div>';
-        var html = '<a href="farmer.html"><img src="img/about_2.png"></img></a><a href="farmer.html" id="name">'+e.Name+'</a><br><a href="farmer.html" id="product">'+e.Name+'</a>';
-        //$('#content').html("");
+        queryP.limit(2);
+        queryP.descending("createdAt");
+        queryP.equalTo("Farmer",e.objectId);
+        queryP.find({
+          success: function(output){
+            var productList = output.map(function (e){ return e.toJSON() });
+            productList.forEach(function (e)){
+              list = list+e.Prod_name+" ";
+            }
+          }
+        });
+        var html = '<a href="farmer.html"><img src="img/about_2.png"></img></a><a href="farmer.html" id="name">'+e.Name+'</a><br><a href="farmer.html" id="product">'+list+'</a>';
         $('.content').append(html);
       });
       //document.getElementById('content').innerHTML = templates.catalogTemplate(objList);
