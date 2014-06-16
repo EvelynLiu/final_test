327875ca1517e6a8ae508a212a95afc063050ef3
EvelynLiu <100703007@nccu.edu.tw>
2014-06-17 03:36:21 +0800

    test parse find then

--- farmer.js
+++ farmer.js
@@ -27,7 +27,8 @@ function call (id){
           console.log(productList);
           productList.forEach(function (e){
             list = e.Prod_name;
-            var pro = '<div class="product"><img src="'+e.Prod_Pic.url'"></img><h3>'+e.Prod_name+'</h3><br><a class="name">'+e.Prod_stat+'</a><p>$'+e.Prod_price+'</p></div>';
+            //var pro = '<div class="product"><img src="'+e.Prod_Pic.url'"></img><h3>'+e.Prod_name+'</h3><br><a class="name">'+e.Prod_stat+'</a><p>$'+e.Prod_price+'</p></div>';
+          	var pro = '<p>ioio</p>';
           	$('.content').append(pro);
           });
         }
