9158425a727bbff60ba14fffa89465c4391bc892
EvelynLiu <100703007@nccu.edu.tw>
2014-06-17 21:15:24 +0800

    test product

--- farmer_list.js
+++ farmer_list.js
@@ -164,7 +164,7 @@ function callprod (id){
   var query = new Parse.Query(Product);
   var queryF = new Parse.Query(Farmer);
   var list="";
-  var id="";
+  var farmerId="";
   query.equalTo("objectId", id);
   query.find({
     success: function(results) {
@@ -176,10 +176,10 @@ function callprod (id){
         success: function(output){
           var farmer = output.map(function (e){ return e.toJSON() });
           list = farmer.Name;
-          id = farmer.objectId;
+          farmerId = farmer.objectId;
         }
       }).then(function(){
-          var detail = '<img src="'+objList[0].Prod_Pic.url+'" id="product-img"><div class="product-info"><h2 id="product-title">'+objList[0].Name+'</h2><p id="product-catogory">'+objList.Category+'</p><ul><p id="product-price">'+objList[0].Prod_stat+' '+objList.Prod_price+'元</p><li class="detail-title">規格</li>'+objList[0].Prod_stat+'，紙箱裝<li class="detail-title">運送方式</li>'+objList[0].Prod_arrive+'，200元<li class="detail-title">付費方式</li>'+objList[0].Prod_payment+'</ul><a id="button" href="farmer.html?name='+id+'">立刻購買</a></div>';
+          var detail = '<img src="'+objList[0].Prod_Pic.url+'" id="product-img"><div class="product-info"><h2 id="product-title">'+objList[0].Name+'</h2><p id="product-catogory">'+objList.Category+'</p><ul><p id="product-price">'+objList[0].Prod_stat+' '+objList.Prod_price+'元</p><li class="detail-title">規格</li>'+objList[0].Prod_stat+'，紙箱裝<li class="detail-title">運送方式</li>'+objList[0].Prod_arrive+'，200元<li class="detail-title">付費方式</li>'+objList[0].Prod_payment+'</ul><a id="button" href="farmer.html?name='+farmerId+'">立刻購買</a></div>';
           $('.product-detail').append(detail);
 
         });
