cad0b8c34b2d4f42e61853cf1847a647369294e5
EvelynLiu <100703007@nccu.edu.tw>
2014-06-17 17:33:15 +0800

    test call farmer

--- farmer_list.js
+++ farmer_list.js
@@ -3,42 +3,6 @@ $(document).ready(function() {
 })
 
 function getData(page,category,tab){
-  alert(0);
-  if(tab==="tab1"){
-    $('#tab1').attr("checked","checked");
-    $('#tab2').removeAttr("checked");
-    $('#tab3').removeAttr("checked"); 
-    $('#tab4').removeAttr("checked");  
-    $('#tab5').removeAttr("checked");
-  }
-  else if(tab==="tab2"){
-    $('#tab2').attr("checked","checked");
-    $('#tab1').removeAttr("checked");
-    $('#tab3').removeAttr("checked"); 
-    $('#tab4').removeAttr("checked");  
-    $('#tab5').removeAttr("checked"); 
-  }
-  else if(tab==="tab3"){
-    $('#tab3').attr("checked","checked");
-    $('#tab2').removeAttr("checked");
-    $('#tab1').removeAttr("checked"); 
-    $('#tab4').removeAttr("checked");  
-    $('#tab5').removeAttr("checked"); 
-  }
-  else if(tab==="tab4"){
-    $('#tab4').attr("checked","checked");
-    $('#tab2').removeAttr("checked");
-    $('#tab3').removeAttr("checked"); 
-    $('#tab1').removeAttr("checked");  
-    $('#tab5').removeAttr("checked"); 
-  }
-  else if(tab==="tab5"){
-    $('#tab5').attr("checked","checked");
-    $('#tab2').removeAttr("checked");
-    $('#tab3').removeAttr("checked"); 
-    $('#tab4').removeAttr("checked");  
-    $('#tab1').removeAttr("checked"); 
-  }
   var limit = 15;
   var skip = (page-1) * limit;
   var Farmer = Parse.Object.extend("Farmer");
@@ -77,29 +41,26 @@ function getData(page,category,tab){
           $('.content').append(html);
         });
       });
-      //document.getElementById('content').innerHTML = templates.catalogTemplate(objList);
+      //底下的小分頁=================
       query.limit(0);
       query.skip(0);
       var option = {};
-      // To support pagination.
       query.count({
         success: function(count){
         var totalPage = Math.ceil(count / limit);
         var currentPage = parseInt(page);
         option = {
-          // Watch out the limit.
           'previous': (currentPage === 1) ? 1 : currentPage-1,
           'next': (currentPage === totalPage) ? currentPage : currentPage+1,
           'current': currentPage,
           'last': totalPage,
         };
-        //document.getElementById('pagination').innerHTML = 
-        //templates.catalogPaginationTemplate(option);
         }, 
         error: function(err){
 
         }  
       });
+      //===========================
     }
   });
   event.preventDefault();
@@ -107,8 +68,6 @@ function getData(page,category,tab){
 
 
 function call (id){
-  //window.location.href = 'farmer.html';
-  alert(0);
   var Farmer = Parse.Object.extend("Farmer");
   var Product = Parse.Object.extend("Product");
   var query = new Parse.Query(Farmer);
@@ -116,32 +75,27 @@ function call (id){
   query.equalTo("objectId", id);
   query.find({
     success: function(results) {
-      alert(1);
-      //$('.content').html("");
       var objList = results.map(function (e){ return e.toJSON() });
-      //objList.forEach(function (e){
-        queryP.descending("createdAt");
-        queryP.equalTo("Farmer",id);
-        queryP.find({
-          success: function(output){
-            var productList = output.map(function (e){ return e.toJSON() });
-            productList.forEach(function (e){
-              var pro = '<div class="product"><img src="'+e.Prod_Pic.url+'"></img><h3>'+e.Prod_name+'</h3><br><a class="name">'+e.Prod_stat+'</a><p>$'+e.Prod_price+'</p></div>';
-              $('.content_p').append(pro);
-              console.log(pro);
-            })
-          }
-        }).then(function(){
-            //var html = '<a href="farmer.html" onClick=" "><div class="about"><img src="'+e.Farmer_Pic.url+'"></img><p class="name">'+e.Name+'</p><p>'+list+'</p></div></a>';
-            var banner = '<img src="'+objList[0].Farmer_Pic.url+'" class="farmer_head"><h2 class="slogan"><div id="farm-name">'+objList[0].Name+'</div><div id="farmer-name">'+objList[0].Name+'</div></h2>';
-            $('.banner').append(banner);
-            var description = '<p>'+objList[0].farm_story+'</p>';
-            $('.description').append(description);
-            var info = '<p><i class="fa fa-home fa-2x"></i><a href="'+objList[0].website+'">'+objList[0].Name+'</a></p><p><i class="fa fa-facebook-square fa-2x"></i><a href="'+objList[0].facebook+'">粉絲專頁</a></p><p><i class="fa fa-phone fa-2x"></i>'+objList[0].telephone+'</p>';
-            $('.info').append(info);
-          });
-        //});
-      }
+      queryP.descending("createdAt");
+      queryP.equalTo("Farmer",id);
+      queryP.find({
+        success: function(output){
+          var productList = output.map(function (e){ return e.toJSON() });
+          productList.forEach(function (e){
+            var pro = '<div class="product"><img src="'+e.Prod_Pic.url+'"></img><h3>'+e.Prod_name+'</h3><br><a class="name">'+e.Prod_stat+'</a><p>$'+e.Prod_price+'</p></div>';
+            $('.content_p').append(pro);
+            console.log(pro);
+          })
+        }
+      }).then(function(){
+          var banner = '<img src="'+objList[0].Farmer_Pic.url+'" class="farmer_head"><h2 class="slogan"><div id="farm-name">'+objList[0].Name+'</div><div id="farmer-name">'+objList[0].Name+'</div></h2>';
+          $('.banner').append(banner);
+          var description = '<p>'+objList[0].farm_story+'</p>';
+          $('.description').append(description);
+          var info = '<p><i class="fa fa-home fa-2x"></i><a href="'+objList[0].website+'">'+objList[0].Name+'</a></p><p><i class="fa fa-facebook-square fa-2x"></i><a href="'+objList[0].facebook+'">粉絲專頁</a></p><p><i class="fa fa-phone fa-2x"></i>'+objList[0].telephone+'</p>';
+          $('.info').append(info);
+        });
+    }
   });
   event.preventDefault();
 }
